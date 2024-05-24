6ed8d551eac762cf3dee048ed2118aba3b9c6f52
oMo <106488685+omiko-dev@users.noreply.github.com>
2024-05-24 19:52:47 +0400

    end
    end

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject pipesPrefab;  // Changed to pipesPrefab to avoid confusion
+    public float spawnRate = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 2f;
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn()
+    {
+        if (pipesPrefab != null)
+        {
+            GameObject pipes = Instantiate(pipesPrefab, transform.position, Quaternion.identity);
+            pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+        }
+        else
+        {
+            Debug.LogWarning("Pipes prefab is missing!");
+        }
+    }
+
+}
